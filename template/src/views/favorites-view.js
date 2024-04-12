8fad3ecfbad64b0437b71163c4de928ec4587fd1
StanislavaGeorgieva <stanislava.georgieva.a58@learn.telerikacademy.com>
2024-04-12 21:28:39 +0300

    change-name

--- template/src/views/favorites-view.js
+++ template/src/views/favorites-view.js
@@ -2,7 +2,7 @@ import { toMovieSimple } from './movie-views.js';
 
 export const toFavoritesView = (movies) => `
 <div id="movies">
-  <h1>Favorite movies:</h1>
+  <h1>Favorite giphy:</h1>
   <div class="content">
     ${movies.map(toMovieSimple).join('\n') || '<p>Add some movies to favorites to see them here.</p>'}
   </div>
