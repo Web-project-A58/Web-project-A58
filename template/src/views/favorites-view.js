e93ad63549555624a7f9bb7ef2d7fe45035b3dad
StanislavaGeorgieva <stanislava.georgieva.a58@learn.telerikacademy.com>
2024-04-13 11:30:09 +0300

    Revert "full-heart"
    This reverts commit 9e0c887761950fde28962589143dc969634f5d1b.

--- template/src/views/favorites-view.js
+++ template/src/views/favorites-view.js
@@ -3,7 +3,7 @@ import { gifDetailedView } from "./gif-details-view.js";
 export const toFavoritesView = (gif) => `
 <div id="movies">
   <h1>Favorite giphy:</h1>
-  <div class="content"> 
+  <div class="content">
     ${gif.map(gifDetailedView).join('\n') || '<p>Add some Giphy to favorites to see them here.</p>'}
   </div>
 </div>
--- template/src/views/home-view.js
+++ template/src/views/home-view.js
@@ -1,16 +1,10 @@
-import { renderFavoriteStatus } from "../events/favorites-events.js";
-
 export const toHomeView = (gifs) => `
 <div id="trending-gifs">
     <h3>Trending</h3>
     ${gifs.map((gif) => {
-      return `
-        <div class="gif-container" data-movie-id="${gif.id}">
-          <img class="img" src="${gif.images.original.url}" alt="${gif.title}">
-          ${renderFavoriteStatus(gif.id)}
-        </div>
-      `;
-    }).join('')} 
+      return `<img id=${gif.id} class="img" src=
+      ${gif.images.original.url}>`;
+    })} 
 </div>
 `;
 
