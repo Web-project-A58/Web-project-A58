7265a342ada02f2c027854306dc83b7e5f0acf7a
StanislavaGeorgieva <stanislava.georgieva.a58@learn.telerikacademy.com>
2024-04-14 10:26:40 +0300

    trendin-gif-delete-comma

--- template/src/views/home-view.js
+++ template/src/views/home-view.js
@@ -4,11 +4,8 @@ export const toHomeView = (gifs) => `
 <div id="trending-gifs">
     <h3>Trending</h3>
     ${gifs.map((gif) => {
-      return `<img id=${gif.id} class="img" src=
-      ${gif.images.original.url}>
-      ${renderFavoriteStatus(gif.id)}
-    `;
-    })} 
+      return `<img id=${gif.id} class="img" src=${gif.images.original.url}>
+      ${renderFavoriteStatus(gif.id)}`;
+    }).join('')} 
 </div>
 `;
-
